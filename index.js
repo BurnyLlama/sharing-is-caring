d33539f9270deea0a9cd47ddfe261f11673b5b78
BobZaa <bobzaagameplay@gmail.com>
2023-01-16 14:04:08 +0100

    I dunno... Plz fix!

--- index.js
+++ index.js
@@ -31,5 +31,6 @@ server.use(bodyParser.urlencoded({ extended: true }))
 
 server.use("/", routes)
 server.use("/static", express.static("static"))
+server.get("/paste", (req, res) => res.render("pages/paste.njk"))
 
 server.listen(12345, () => log.write("Listening on 'http://localhost:12345/'!"))
