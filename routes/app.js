5d6bd3423e0e813fc1508737b90968e6fa53a0ee
BobZaa <bobzaagameplay@gmail.com>
2023-01-23 12:23:49 +0100

    commit murder

--- routes/app.js
+++ routes/app.js
@@ -5,5 +5,6 @@ const app = Router()
 app.get("/", (req, res) => res.render("pages/landing.njk"))
 app.get("/paste", (req, res) => res.render("pages/paste.njk"))
 app.get("/file", (req, res) => res.render("pages/file.njk"))
+app.get("/custom", (req,res) => res.render("pages/custom.njk"))
 
 export default app
