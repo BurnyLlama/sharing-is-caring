d69396e4e1a00a506741a89b7f8312d86898c87e
Fabian Seluska <fabianseluska@gmail.com>
2023-01-23 12:32:45 +0100

    Can now view paste.

--- routes/app.js
+++ routes/app.js
@@ -3,8 +3,8 @@ import { Router } from "express"
 const app = Router()
 
 app.get("/", (req, res) => res.render("pages/landing.njk"))
-app.get("/paste", (req, res) => res.render("pages/paste.njk"))
-app.get("/file", (req, res) => res.render("pages/file.njk"))
+app.get("/paste", (req, res) => res.render("pages/create_paste.njk"))
+app.get("/file", (req, res) => res.render("pages/create_file.njk"))
 app.get("/custom", (req,res) => res.render("pages/custom.njk"))
 
 export default app
